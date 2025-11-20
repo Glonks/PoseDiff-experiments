fix shape broadcasting in DDPMDIffuser fix activation instantiation in PoseDiffModel

components/diffusers/ddpm_diffuser.py
@@ -66,13 +66,12 @@ class DDPMDiffuser(nn.Module):
         """
         Get tensor values in a shape suitable for broadcasting.
         """
-        batch_size, *_ = tensor.shape
+        batch_size, *_ = shape
 
-        out = tensor.gather(-1, indices)
-        out = out.reshape(batch_size, *([1] * (len(shape) - 1)))
-
-        # TODO: test if this works
-        # out = tensor[indices].reshape(batch_size, *([1] * (len(shape) - 1)))
+        out = (
+            tensor[indices]
+            .reshape(batch_size, *([1] * (len(shape) - 1)))
+        )
 
         return out
 

pose_diff.py
@@ -52,11 +52,12 @@ class PoseDiffModel(nn.Module):
 
         # TODO: figure out keypoint_dim from the dataset
         unet_config = config['unet']
+        keypoint_dim = 3  # TODO: read this from config instead
         self.unet = ConditionalUNet(
             keypoint_dim,
             condition_embedder_config['output_dim'],
             unet_config['hidden_dims'],
-            eval(unet_config['activation'])()
+            eval(unet_config['activation'])
         )
         if self.enable_cfg:
             self.unet = CFGWrapper(self.unet, self.guidance_strength)